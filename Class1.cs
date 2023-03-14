48eb31a46dd812c22b2416c045cf3c034aa18b24
Marie <marie.landivar@devsu.com>
2023-03-14 09:39:09 -0500

    Added second command to add value for preselected numbers. Should become one single command in the future.

--- Class1.cs
+++ Class1.cs
@@ -3,11 +3,14 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Colors;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+using System.Diagnostics;
 
 namespace cmd_AutoCAD.text
 {
     public class Class1 : IExtensionApplication
     {
+       
         #region IExtensionApplication Members
         public void Initialize()
         {
@@ -21,8 +24,8 @@ namespace cmd_AutoCAD.text
 
         #endregion
 
-        [CommandMethod("AddValueToTextNumber")]
-        public void ChangeTextColor()
+        [CommandMethod("AddValueToSingleNumber")]
+        public void SumValueToText()
         {
             // Get the current document and database
             Document doc = Application.DocumentManager.MdiActiveDocument;
@@ -31,12 +34,28 @@ namespace cmd_AutoCAD.text
             // Get the editor and prompt the user to select a text element
             Editor ed = doc.Editor;
 
-            PromptDoubleOptions valueOptions = new PromptDoubleOptions("\nEnter the value to add to the text: ");
-            valueOptions.AllowZero = true;
-            valueOptions.AllowNegative = true;
-            PromptDoubleResult valueResult = ed.GetDouble(valueOptions);
-            if (valueResult.Status != PromptStatus.OK)
-                return;
+            PromptDoubleOptions inputOptions = new PromptDoubleOptions("\nEnter value to be added or : ")
+            {
+                AllowZero = true,
+                AllowNegative = true,
+
+            };
+
+            string keywordColor = "Color";
+            // Add a keyword option to the prompt
+            inputOptions.Keywords.Add(keywordColor);
+
+            // Get the value entered by the user
+            PromptDoubleResult inputResult = ed.GetDouble(inputOptions);
+            if (inputResult.Status == PromptStatus.Keyword)
+            {
+                if (inputResult.StringResult == keywordColor)
+                {
+                    Application.ShowAlertDialog("Entered keyword: " + inputResult.StringResult);
+                }
+                
+            }
+
 
             // Loop until the user cancels or ends the command
             while (true)
@@ -54,10 +73,9 @@ namespace cmd_AutoCAD.text
                     DBText text = (DBText)tr.GetObject(result.ObjectId, OpenMode.ForWrite);
 
                     // Convert the text to a number
-                    double number;
                     int decimalPlaces = 0;
                     string resultText;
-                    if (!double.TryParse(text.TextString, out number))
+                    if (!double.TryParse(text.TextString, out double number))
                     {
                         ed.WriteMessage("\nThe selected text element does not contain a number.");
                         return; // Esto causa que el método termine
@@ -72,7 +90,7 @@ namespace cmd_AutoCAD.text
 
 
                     // Add a value to the number
-                    double valueToAdd = valueResult.Value; // Change this to the value you want to add
+                    double valueToAdd = inputResult.Value; // Change this to the value you want to add
                     double resultValue = number + valueToAdd;
 
                     //Convert number to text
@@ -93,6 +111,137 @@ namespace cmd_AutoCAD.text
 
                 }
             }
+        }
+
+
+        [CommandMethod("AddValueToMultipleNumbers", CommandFlags.UsePickSet)]
+        public void SumValueMultipleText()
+        {
+            // Get the current document and database
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+
+            // Get the editor and prompt the user to select a text element
+            Editor ed = doc.Editor;
+
+            // Get the previously selected objects
+            PromptSelectionResult psr = ed.SelectImplied();
+            if (psr.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nNo objects were previously selected.");
+                return;
+            }
+
+            SelectionSet ss = psr.Value;
+            ObjectId[] objIds = ss.GetObjectIds();
+
+            PromptDoubleOptions inputOptions = new PromptDoubleOptions("\nEnter value to be added or : ")
+            {
+                AllowZero = true,
+                AllowNegative = true,
+
+            };
+
+            string keywordColor = "Color";
+            // Add a keyword option to the prompt
+            inputOptions.Keywords.Add(keywordColor);
+
+            // Get the value entered by the user
+            PromptDoubleResult inputResult = ed.GetDouble(inputOptions);
+            if (inputResult.Status == PromptStatus.Keyword)
+            {
+                if (inputResult.StringResult == keywordColor)
+                {
+                    Application.ShowAlertDialog("Entered keyword: " + inputResult.StringResult);
+                }
+
+            }
+
+            // Open the selected text element for write
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                foreach (ObjectId objId in objIds)
+                {
+                    DBText text = (DBText)tr.GetObject(objId, OpenMode.ForWrite);
+
+                    // Convert the text to a number
+                    int decimalPlaces = 0;
+                    string resultText;
+                    if (!double.TryParse(text.TextString, out double number))
+                    {
+                        ed.WriteMessage("\nThe selected text element does not contain a number.");
+                        return; // Esto causa que el método termine
+                    }
+
+                    // Count the number of decimal places
+                    int decimalIndex = text.TextString.IndexOf(".");
+                    if (decimalIndex >= 0)
+                    {
+                        decimalPlaces = text.TextString.Length - decimalIndex - 1;
+                    }
+
+                    // Add a value to the number
+                    double valueToAdd = inputResult.Value; // Change this to the value you want to add
+                    double resultValue = number + valueToAdd;
+
+                    //Convert number to text
+                    resultText = resultValue.ToString("0." + new string('0', decimalPlaces));
+
+                    // Add a value to the text
+                    text.TextString = resultText;
+
+                    // Set the color of the text entity to purple.
+                    Color purple = Color.FromColorIndex(ColorMethod.ByAci, 211);
+                    text.Color = purple;
+
+                    // Save the changes made to the text entity.
+                    text.RecordGraphicsModified(true);
+                }
+                // Commit the transaction
+                tr.Commit();
+
+
+            }
+            
+        }
+
+        [CommandMethod("RenameAnonBlocks", CommandFlags.UsePickSet)]
+        public void RenameAnonBlocks()
+        {
+            // Get the current document and database
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+
+            // Get the editor and prompt the user to select a text element
+            Editor ed = doc.Editor;
+
+            // Get the previously selected objects
+            PromptSelectionResult psr = ed.SelectImplied();
+            if (psr.Status != PromptStatus.OK)
+            {
+                ed.WriteMessage("\nNo objects were previously selected.");
+                return;
+            }
+
+            SelectionSet ss = psr.Value;
+            ObjectId[] anonymousBlockIds = ss.GetObjectIds();
+
+            // Open the selected text element for write
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                int number = 0;
+                foreach (ObjectId anonymousBlockId in anonymousBlockIds)
+                {
+                    BlockTableRecord anonymousBlock = (BlockTableRecord)tr.GetObject(anonymousBlockId, OpenMode.ForWrite);
+                    anonymousBlock.Name = number.ToString();
+                    number += 1;
+                }
+                // Commit the transaction
+                tr.Commit();
+
+
+            }
+
         }
 
     }

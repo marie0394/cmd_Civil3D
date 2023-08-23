da7472b34c84004453b821623f0d916857fa69e8
Marie <marie.landivar@devsu.com>
2023-08-23 01:58:22 -0500

    Added CreateNewLabelStyle method

--- PointCommands.cs
+++ PointCommands.cs
@@ -7,6 +7,7 @@ using Autodesk.Civil.DatabaseServices;
 using Autodesk.Civil.ApplicationServices;
 using Autodesk.AutoCAD.Colors;
 using System.Xml.Linq;
+using Autodesk.Civil.DatabaseServices.Styles;
 
 namespace cmd_C3D
 {
@@ -32,12 +33,14 @@ namespace cmd_C3D
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Database db = doc.Database;
             Editor ed = doc.Editor;
+            CivilDocument civilDocument = CivilApplication.ActiveDocument;
 
             using (Transaction tr = db.TransactionManager.StartTransaction())
             {
                 // Access the COGO points collection
                 CogoPointCollection cogoPoints = CogoPointCollection.GetCogoPoints(db);
                 PointGroupCollection pointGroups = PointGroupCollection.GetPointGroups(db);
+                LabelStyleCollection pointLabelStyles = civilDocument.Styles.LabelStyles.PointLabelStyles.LabelStyles;
 
                 // Create a set to store unique description values
                 HashSet<string> uniqueDescriptions = new HashSet<string>();
@@ -60,19 +63,28 @@ namespace cmd_C3D
                 {
                     // ObjectId newPointGroup = pointGroups.Add(uniqueDescription);
                     Color purple = Color.FromColorIndex(ColorMethod.ByAci, 211);
-                    CreateNewPointGroup(uniqueDescription, purple, pointGroups);
+                    ObjectId newPointGroupId = CreateNewPointGroup(uniqueDescription, purple, pointGroups);
+                    ObjectId newPointLabelStyleId = CreateNewLabelStyle(uniqueDescription, purple, pointLabelStyles);
+                    PointGroup pointGrp = newPointGroupId.GetObject(OpenMode.ForWrite) as PointGroup;
+                    pointGrp.PointLabelStyleId = newPointLabelStyleId;
                 }
-
-
+                
 
                 tr.Commit();
 
             }
         }
 
-        public static void CreateNewPointGroup(string name, Color color, PointGroupCollection pointGroups)
+        public static ObjectId CreateNewPointGroup(string name, Color color, PointGroupCollection pointGroups)
         {
-            ObjectId newPointGroup = pointGroups.Add(name);
+            ObjectId newPointGroupId = pointGroups.Add(name);
+            return newPointGroupId;
+        }
+
+        public static ObjectId CreateNewLabelStyle(string name, Color color, LabelStyleCollection pointLabelStyles)
+        {
+            ObjectId newPointLabelStyleId = pointLabelStyles.Add(name);
+            return newPointLabelStyleId;
         }
     }
 }
